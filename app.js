401a7f06d43e7553749b68a9dad1f3443f795a6d
Ramdas Gupta <er.rdgupta@gmail.com>
2023-05-16 09:37:51 +0530

    cors issue resolved

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express()
 // middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // route
 const routes = require('./Routes/Route')
 app.use('/', routes)
